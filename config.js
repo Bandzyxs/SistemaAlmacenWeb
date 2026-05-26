76dd41479a17614558a6f4ec116d576c684f361d
Bandzyxs <155047688+Bandzyxs@users.noreply.github.com>
2026-05-25 21:57:59 -0600

    Remove sensitive API keys from config.js
    Removed sensitive configuration data from config.js.

--- config.js
+++ config.js
@@ -1,11 +1,1 @@
-const CONFIG_SECRETA = {
-    PIXABAY_API_KEY: '56024048-9ab7a0dafe14e06f31baf7acc',
-    FIREBASE: {
-        apiKey: "AIzaSyB2-3u4nIglOakKcvQzWJNQDs6xrQT47Uk",
-        authDomain: "sistema-almacen-1088d.firebaseapp.com",
-        projectId: "sistema-almacen-1088d",
-        storageBucket: "sistema-almacen-1088d.firebasestorage.app",
-        messagingSenderId: "572823961320",
-        appId: "1:572823961320:web:8b772b838986d323f4935d"
-    }
-};
+
